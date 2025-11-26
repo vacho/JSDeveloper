a61cf418bc6b6b79f965d6bfa92ae6a74182931b
vacho <vachisinio@gmail.com>
2025-11-26 10:01:58 -0400

    [js] OOP with constructor functions - Objects!

--- Examples/oop/function.js
+++ Examples/oop/function.js
@@ -1,3 +1,4 @@
+// This is useful to create directly objects and override their behavior individually.
 function Person(){
     this.age = 12;
     this.talk = function() {
@@ -7,17 +8,19 @@ function Person(){
 
 }
 /*
+// To add methods to the prototype (to be inheritable)
 Person.prototype.talk = function() {
     console.log("Hello, I am a person.");
 }*/
 
 const me = new Person();
 me.talk();
-//Person.age = 40;
-//me.age; 
-//Person.prototype.age = 30;
 me.age; 
 
-/*const you = new Person();
+const you = new Person();
+you.talk = function() {
+    console.log("Hello, I am you.");
+}
 you.talk();
-*/
+me.talk();
+
