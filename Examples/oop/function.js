e0875fdcffcb623ea3ae4a0e701046b4ca11aa97
vacho <vachisinio@gmail.com>
2025-11-26 13:19:23 -0400

    [js] OOP constructor and factory functions

--- Examples/oop/function.js
+++ Examples/oop/function.js
@@ -1,26 +1,46 @@
 // This is useful to create directly objects and override their behavior individually.
-function Person(){
-    this.age = 12;
-    this.talk = function() {
-        console.log("Hello, I am a person.");
+
+// Constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = function() {
+        console.log(`CF Hello, my name is ${name} and I am ${age} years old.`);
     }
-
-
 }
 /*
-// To add methods to the prototype (to be inheritable)
-Person.prototype.talk = function() {
+// Another way to add methods to the prototype (to be inheritable)
+Person.prototype.greet = function() {
     console.log("Hello, I am a person.");
 }*/
 
-const me = new Person();
-me.talk();
-me.age; 
+const p1 = new Person('Osvaldo', 45);
+p1.greet();
 
-const you = new Person();
-you.talk = function() {
-    console.log("Hello, I am you.");
+
+// Altering the behavior of a single object.
+const p2 = new Person('Aidee', 46);
+p2.greet = function() {
+    console.log(`CF Hello, I am ${this.name}.`);
 }
-you.talk();
-me.talk();
+p2.greet();
+p1.greet();
+
+
+// Factory function
+function createPerson(name, age) {
+  return {
+    name: name,
+    age: age,
+    greet: function() {
+      console.log(`FF Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  };
+}
+
+// Using the factory function to create new objects
+const person1 = createPerson("Alice", 30);
+person1.greet();
+/*const person2 = createPerson("Bob", 25);
+person2.greet();*/
 
