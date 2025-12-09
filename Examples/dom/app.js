0263edb79486382eb1ec5182cbf3fdae182d3ae1
vacho <vachisinio@gmail.com>
2025-12-09 15:40:19 -0400

    [js] dom basic

--- Examples/dom/app.js
+++ Examples/dom/app.js
@@ -1,18 +1,55 @@
 // This code runs only in a browser environment.
-
 document.body.innerText = '';
+const form = document.createElement('form');
+document.body.appendChild(form);
 
 const input = document.createElement('input');
 input.placeholder = 'To do';
-document.body.appendChild(input);
+form.appendChild(input);
 
 const button = document.createElement('button');
 button.innerText = "Add";
-document.body.appendChild(button);
+button.role = "submit";
+form.appendChild(button);
 
-button.addEventListener('click', () => {
-   let p = document.createElement('p');
-   p.innerText = input.value;
-   document.body.appendChild(p);
+const ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+const removeElement = el => {
+   el.remove();
+}
+
+function removeEl(el) {
+   el.remove();
+}
+
+form.onsubmit = (e) => {
+   e.preventDefault();
+   const li = document.createElement('li');
+   li.innerText = input.value;
+   ul.appendChild(li);
    input.value = '';
-});
+
+   //li.onclick = removeElement.bind(this, li);
+   //li.onclick = () => removeEl(li);
+   li.onclick = removeEl.bind(this, li);
+};
+
+
+// Styling
+document.body.style.padding = '50px';
+input.style.marginRight = '10px'
+document.body.style.backgroundColor = '#f0f0f0';
+document.body.setAttribute('class', 'blue');
+document.getElementsByClassName('blue');
+document.querySelector('.blue');
+
+function removeSome(word) {
+    const list = Array.from(document.querySelectorAll('li'));
+    list.forEach(li => {
+        if(li.innerText.includes(word)) {
+            li.remove();
+        }
+    })  
+};
+removeSome('r');
