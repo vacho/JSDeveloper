bb5b195aa1bf3ae63c31c317a9a2242d1f89c8c1
vacho <vachisinio@gmail.com>
2025-12-19 10:46:34 -0400

    [js][typescript] Objects

--- Typescript/Examples/Objects/dist/index.js
+++ Typescript/Examples/Objects/dist/index.js
@@ -1,14 +1,33 @@
-const employee = {
-    id: 124,
-    name: "John Doe",
-    age: 30,
-    retire: (date) => {
-        console.log(`Employee will retire on ${date}`);
-    }
-};
+let employees = [
+    {
+        id: 124,
+        name: "John Doe",
+        age: 30,
+        retire: (date) => {
+            console.log(`Employee will retire on ${date}`);
+        }
+    },
+    {
+        id: 123,
+        name: "Jennifer Lopez",
+        age: 20,
+        retire: (date) => {
+            return date;
+        }
+    },
+];
 // We can't change id because it's readonly.
 //employee.id = 1;
-console.log(employee);
-console.log(employee.retire(new Date()));
+console.log(employees);
+employees.forEach((emp) => {
+    console.log(`Employee Name: ${emp.name}, Age: ${emp.age}`);
+});
+if (employees[0]) {
+    employees[0].retire(new Date());
+}
+if (employees[1]) {
+    let dateFinish = '2029';
+    console.log(`The employee ${employees[1].name} will retire on: ${employees[1].retire(dateFinish)}`);
+}
 export {};
 //# sourceMappingURL=index.js.map
--- Typescript/Examples/Objects/src/index.ts
+++ Typescript/Examples/Objects/src/index.ts
@@ -1,19 +1,41 @@
-const employee: {
-    readonly id:number, 
-    name:string,
-    age?: number,
-    retire: (date: Date) => void
-} = {
-    id: 124, 
-    name: "John Doe",
-    age: 30,
-    retire: (date: Date) => {
-        console.log(`Employee will retire on ${date}`);
-    }
+type Employee = {
+    readonly id: number;
+    name: string;
+    age: number;
+    retire: (date: any) => any;
 };
 
+let employees: Employee[] = [
+    {
+        id: 124, 
+        name: "John Doe",
+        age: 30,
+        retire: (date: Date) => {
+            console.log(`Employee will retire on ${date}`);
+        }
+    },
+    {
+        id: 123, 
+        name: "Jennifer Lopez",
+        age: 20,
+        retire: (date: String) => {
+            return date;
+        }
+    },
+];
 // We can't change id because it's readonly.
 //employee.id = 1;
 
-console.log(employee);
-console.log(employee.retire(new Date()));
+console.log(employees);
+
+employees.forEach((emp: Employee) => {
+    console.log(`Employee Name: ${emp.name}, Age: ${emp.age}`);
+});
+if (employees[0]) {
+    employees[0].retire(new Date());
+}
+
+if (employees[1]) {
+    let dateFinish = '2029';
+    console.log(`The employee ${employees[1].name} will retire on: ${employees[1].retire(dateFinish)}`);
+}
